e23440e68a8e3792fe6b2a5a8924d732d0cb3331
Tien2403 <vantien20042004@gmail.com>
2026-01-09 20:47:02 +0700

    hoan thien profile

--- src/main/java/com/shopsense/security/SecurityConfig.java
+++ src/main/java/com/shopsense/security/SecurityConfig.java
@@ -22,19 +22,19 @@ public class SecurityConfig {
 
 	@Autowired
 	JwtAuthFilter jwtAuthFilter;
-	
+
 	@Autowired
 	AuthProvider authProvider;
-	
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));
-        
+		corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+		corsConfiguration.setAllowedOrigins(List.of("*"));
+		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
 		return http
 				.csrf(csrf -> csrf.disable())
 				.cors(Customizer.withDefaults())
